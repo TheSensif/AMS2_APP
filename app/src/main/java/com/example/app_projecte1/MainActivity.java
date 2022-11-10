22cf8788e6a8d5406fb667d7f07123917a36eac5
DavidIbanez <davidibanezmoya@gmail.com>
2022-11-10 18:33:30 +0100

    feat:set incorrect message

--- app/src/main/java/com/example/app_projecte1/MainActivity.java
+++ app/src/main/java/com/example/app_projecte1/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.app_projecte1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //uu
+
+        Button buttonLogIn = findViewById(R.id.buttonLogIn);
+        EditText userInput = findViewById(R.id.editUsername);
+        EditText passInput = findViewById(R.id.editPassword);
+        EditText ipInput = findViewById(R.id.editServerIp);
+
+        String user = "Test";
+        String passwd = "test1234";
+        String ip = "192.168.1.1";
+
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                if (!user.equals(userInput.getText().toString()) ) {
+                    Log.v("msg","User is incorrect");
+                    String text = "User or password is incorrect";
+                    message(text);
+                }
+                if (!passwd.equals(passInput.getText().toString())) {
+                    Log.v("msg","The password is incorrect");
+                    String text = "User or password is incorrect";
+                    message(text);
+                }
+
+
+
+            }
+        });
+
+
     }
+
+    public void message(String text) {
+        Snackbar sbMsg = Snackbar.make(findViewById(R.id.constraintLayout),text,3000);
+        sbMsg.show();
+        Log.v("funcion","Msg en la funcion");
+    };
 }
