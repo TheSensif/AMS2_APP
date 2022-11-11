5e4f0937eb2017490bfbeb2faef651a52c824900
DavidIbanez <davidibanezmoya@gmail.com>
2022-11-11 18:57:58 +0100

    feat:start new activity

--- app/src/main/java/com/example/app_projecte1/MainActivity.java
+++ app/src/main/java/com/example/app_projecte1/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.app_projecte1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,25 +26,37 @@ public class MainActivity extends AppCompatActivity {
         EditText passInput = findViewById(R.id.editPassword);
         EditText ipInput = findViewById(R.id.editServerIp);
 
-        String user = "Test";
-        String passwd = "test1234";
-        String ip = "192.168.1.1";
+
+        String user = "admin";
+        String passwd = "1234";
 
         buttonLogIn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
+                Boolean error = false;
+
                 if (!user.equals(userInput.getText().toString()) ) {
-                    Log.v("msg","User is incorrect");
                     String text = "User or password is incorrect";
-                    message(text);
+                    error = message(text);
+                    userInput.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+
                 }
                 if (!passwd.equals(passInput.getText().toString())) {
-                    Log.v("msg","The password is incorrect");
                     String text = "User or password is incorrect";
-                    message(text);
+                    error = message(text);
+                    passInput.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
                 }
 
+                if (ipInput.getText().toString().equals("")) {
+                    String text = "Server IP is null!";
+                    error = message(text);
+                    ipInput.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+                }
 
+                if (!error) {
+                    Intent newMain = new Intent(MainActivity.this,ieti_industry.class);
+                    startActivity(newMain);
+                }
 
             }
         });
@@ -48,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void message(String text) {
+    public Boolean message(String text) {
         Snackbar sbMsg = Snackbar.make(findViewById(R.id.constraintLayout),text,3000);
         sbMsg.show();
-        Log.v("funcion","Msg en la funcion");
+        return true;
     };
 }
---
+++ app/src/main/java/com/example/app_projecte1/ieti_industry.java
@@ -0,0 +1,14 @@
+package com.example.app_projecte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ieti_industry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ieti_industry);
+    }
+}
