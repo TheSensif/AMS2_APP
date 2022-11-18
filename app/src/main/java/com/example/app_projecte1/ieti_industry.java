979cff8b17ab31c7883ca34cf54bdf4760ca6b6e
DavidIbanez <davidibanezmoya@gmail.com>
2022-11-18 19:27:23 +0100

    Trying to read the Json Data with Arrays

--- app/src/main/java/com/example/app_projecte1/ieti_industry.java
+++ app/src/main/java/com/example/app_projecte1/ieti_industry.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ieti_industry extends AppCompatActivity {
 
@@ -34,6 +38,39 @@ public class ieti_industry extends AppCompatActivity {
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset());
             System.out.println(obj);
+            int quantityElements = obj.length();
+
+            List<String> namesList = new ArrayList<>();
+            Iterator<String> stringIterator = obj.keys();
+            while (stringIterator.hasNext()) {
+                namesList.add(stringIterator.next());
+            }
+            for (int i = 0; i < quantityElements; i++) {
+                if (namesList.get(i).equals("switch")) {
+                    JSONArray arrayData = obj.getJSONArray("switch");
+                    System.out.println(arrayData.length());
+                    List<String> namesSwitch = new ArrayList<>();
+
+                    for (int j = 0; j < arrayData.length(); j++) {
+                        System.out.println(arrayData.get(j));
+                        JSONObject switchObj = arrayData.getJSONObject(j);
+                        if (j==0) {
+                            //todo RECORRER ESTO PARA SACAR LAS KEYS DE LOS CAMPOS
+                            stringIterator = obj.keys();
+                            while (stringIterator.hasNext()) {
+                                namesList.add(stringIterator.next());
+                            }
+                        }
+                    }
+                } else if (namesList.get(i).equals("slider")) {
+                    JSONArray arrayData = obj.getJSONArray("slider");
+                }else if (namesList.get(i).equals("dropdown")) {
+                    JSONArray arrayData = obj.getJSONArray("dropdown");
+                }else if (namesList.get(i).equals("sensor")) {
+                    JSONArray arrayData = obj.getJSONArray("sensor");
+
+                }
+                        }
         } catch (JSONException e) {
             e.printStackTrace();
         }
